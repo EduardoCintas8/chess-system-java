7715756c8d9d1d8896aa09887e6e47db37988b79
EduardoCintas8 <cintaseduardo@gmail.com>
2024-06-29 11:08:10 -0300

    Placing pieces on the board

---
+++ src/chess/pieces/King.java
@@ -0,0 +1,17 @@
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class King extends ChessPiece {
+
+	public King(Board board, Color color) {
+		super(board, color);
+	}
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder("K");
+		return sb.toString();
+	}
+}
